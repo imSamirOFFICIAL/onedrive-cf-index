ab54255309ccd00794a8a97f65d6ec7309ff2d63
Samir <asamir192039@bscse.uiu.ac.bd>
2023-11-11 03:30:28 +0600

    Update FolderListLayout.tsx

--- src/components/FolderListLayout.tsx
+++ src/components/FolderListLayout.tsx
@@ -52,20 +52,22 @@ const FolderListLayout = ({
         </div>
       </div>
 
-      {folderChildren.map((c: OdFolderChildren) => (
-        <div
-          className="grid grid-cols-12 transition-all duration-100 hover:bg-gray-100 dark:hover:bg-gray-850"
-          key={c.id}
-        >
-          <Link
-            href={`${path === '/' ? '' : path}/${encodeURIComponent(c.name)}`}
-            passHref
-            className="col-span-12 md:col-span-10"
+      {folderChildren
+        .filter((c: OdFolderChildren) => c.name.endsWith('.mkv') || c.name.endsWith('.mp4'))
+        .map((c: OdFolderChildren) => (
+          <div
+            className="grid grid-cols-12 transition-all duration-100 hover:bg-gray-100 dark:hover:bg-gray-850"
+            key={c.id}
           >
-            <FileListItem fileContent={c} />
-          </Link>          
-        </div>
-      ))}
+            <Link
+              href={`${path === '/' ? '' : path}/${encodeURIComponent(c.name)}`}
+              passHref
+              className="col-span-12 md:col-span-10"
+            >
+              <FileListItem fileContent={c} />
+            </Link>          
+          </div>
+        ))}
     </div>
   )
 }
